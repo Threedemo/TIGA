2d1e122b82c51419e16fbdcee1d9f78a806c3196
邵琪 <1661039206@qq.com>
2020-06-30 09:51:52 +0800

    repostitory

--- SmartCampus/src/main/java/com/three/web2/teacher/repository/TeacherRepository.java
+++ SmartCampus/src/main/java/com/three/web2/teacher/repository/TeacherRepository.java
@@ -1,5 +1,11 @@
 package com.three.web2.teacher.repository;
 
-public class TeacherRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.three.web2.pojo.Teacher;
+
+@Repository
+public interface TeacherRepository extends MongoRepository<Teacher, String> {
 
 }
