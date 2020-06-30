c9c18a8ada64dc743087e4e754797cad602ded5c
陈明明 <@ming.mshome.net>
2020-06-30 09:50:27 +0800

    修改admin仓库名

--- SmartCampus/src/main/java/com/three/web2/admin/repository/AdminRepository.java
+++ SmartCampus/src/main/java/com/three/web2/admin/repository/AdminRepository.java
@@ -1,5 +1,11 @@
 package com.three.web2.admin.repository;
 
-public class AdminRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.three.web2.pojo.Admin;
+
+@Repository
+public interface AdminRepository extends MongoRepository<Admin, String>{
 
 }
