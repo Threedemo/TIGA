e34af075a63f8c250a582342b8bdbe0d1a5367da
邵琪 <1661039206@qq.com>
2020-07-08 09:08:44 +0800

    教师主页

--- SmartCampus/src/main/java/com/three/web2/test/TeacherTest.java
+++ SmartCampus/src/main/java/com/three/web2/test/TeacherTest.java
@@ -1,5 +1,16 @@
 package com.three.web2.test;
 
-public class TeacherTest {
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+public class TeacherTest {
+public static void main(String[] args) {
+	String password="1234";
+	
+	password=new BCryptPasswordEncoder().encode(password);
+	System.out.println(password);
+	
+	boolean falg=new BCryptPasswordEncoder().matches("1234", password);
+	
+	System.out.println(falg);
+}
 }
