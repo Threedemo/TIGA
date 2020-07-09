997c065c179733b8af041f348bb8b5b9c6d14d65
邵琪 <1661039206@qq.com>
2020-07-09 14:25:50 +0800

    111

---
+++ SmartCampus/src/main/java/com/three/web2/teacher/controller/teaController.java
@@ -0,0 +1,14 @@
+package com.three.web2.teacher.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class teaController {
+
+	
+	@GetMapping("/teamag")
+	public String teamsg() {
+		return "stumsg.html";
+	}
+}
